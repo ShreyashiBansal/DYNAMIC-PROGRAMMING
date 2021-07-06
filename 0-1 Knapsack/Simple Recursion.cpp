9cdebbb0a3c75fdeaec7aa65365ebac1cfa06d16
Shreyashi Bansal <65393257+ShreyashiBansal@users.noreply.github.com>
2021-07-06 19:08:43 +0530

    Update Simple Recursion.cpp

--- 0-1 Knapsack/Simple Recursion.cpp
+++ 0-1 Knapsack/Simple Recursion.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int knapSack(int w, int wt[], int val[], int n){
     if(n==0 || w==0)
       return 0;
-     if(wt[n-1]<w){
+     if(wt[n-1]<=w){
          return max(val[n-1]+knapSack(w-wt[n-1], wt, val, n-1),knapSack(w, wt, val, n-1));
      }
      else
@@ -19,3 +19,7 @@ int main()
     cout << knapSack(W, wt, val, n);
     return 0;
 }
+/*
+Time Complexity:O(2^n)
+Space Complexity: O(1)
+*/
